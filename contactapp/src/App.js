813693b14e6094f663f514109517d636d4929dc4
Hassan837 <hassanirfan429@gmail.com>
2021-08-25 01:47:09 +0500

    contact-app

---
+++ contactapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from './components/Main';
+import './App.css';
+
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1 className='text-center' >  Contact-List </h1>
+      <Main></Main>
+      
+    </div>
+  );
+}
+
+export default App;
